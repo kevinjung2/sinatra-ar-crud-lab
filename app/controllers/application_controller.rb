f8af2d18d8e3b2892d043e64f55755f07c31b05d
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-26 22:16:32 -0500

    commit tree 593014e7e44d0c1a1015f65272095b6de94ebb35

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,40 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    a = Article.create(title: params[:title], content: params[:content])
+    redirect :"/articles/#{a.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    a = Article.find_by(id: params[:id])
+    a.title = params[:title]
+    a.content = params[:content]
+    a.save
+    redirect :"/articles/#{a.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.find_by(id: params[:id]).destroy
+  end
 end
